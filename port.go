7a3e4c3e6f9033e043dca184851da0fcaf364347
Rigry <arigry@gmail.com>
2022-01-28 18:56:59 +0300

    work version

--- port.go
+++ port.go
@@ -3,7 +3,6 @@ package debug_uart
 import (
 	"encoding/binary"
 	"log"
-
 	"time"
 
 	"golang.org/x/sys/windows/registry"
@@ -16,7 +15,7 @@ type Uart struct {
 	holdingRegisters []uint16
 	Started          bool
 	Stopped          bool
-	Order            bool
+	switch_charger   bool
 	state            byte
 	error            byte
 }
@@ -42,7 +41,7 @@ func (uart *Uart) Close() {
 
 func (uart *Uart) Start() {
 
-	var packet = []byte{'R', '1', '0', 255}
+	var packet = []byte{'R', '1', 0, 255}
 	uart.port.Write(packet)
 
 	time.Sleep(10 * time.Millisecond)
@@ -53,7 +52,7 @@ func (uart *Uart) Start() {
 
 func (uart *Uart) Stop() {
 
-	var packet = []byte{'R', '0', 255}
+	var packet = []byte{'R', 0, 255}
 	uart.port.Write(packet)
 
 	time.Sleep(10 * time.Millisecond)
@@ -65,7 +64,7 @@ func (uart *Uart) Stop() {
 func (uart *Uart) GetData() []uint16 {
 
 	answer := make([]byte, 25)
-	var packet = []byte{'C', 'V', '0', 255}
+	var packet = []byte{'C', 'V', 0, 255}
 	uart.port.Write(packet)
 	time.Sleep(10 * time.Millisecond)
 	uart.port.Read(answer)
@@ -90,16 +89,39 @@ func (uart *Uart) Get(packet []byte) {
 
 func (uart *Uart) GetState() byte {
 
-	var packet = []byte{'S', 't', '0', 255}
+	var packet = []byte{'S', 't', 0, 255}
 	uart.Get(packet)
 	return uart.state
 }
 
 func (uart *Uart) GetError() byte {
 
-	var packet = []byte{'E', 'r', '0', 255}
+	var packet = []byte{'E', 'r', 0, 255}
 	uart.Get(packet)
 	return uart.error
+}
+
+func (uart *Uart) Charging() {
+	if uart.switch_charger {
+		var packet = []byte{'C', '0', 0, 255}
+		uart.port.Write(packet)
+		uart.switch_charger = false
+	} else {
+		var packet = []byte{'C', '1', 0, 255}
+		uart.port.Write(packet)
+		uart.switch_charger = true
+	}
+}
+
+func (uart *Uart) Balance(n byte) {
+	var packet = []byte{'d', n, 255}
+	uart.port.Write(packet)
+}
+
+func (uart *Uart) Correction(n int, data uint16) {
+	high, low := uint16ToBytes(data)
+	var packet = []byte{'C', 'A', byte(n), high, low, 255}
+	uart.port.Write(packet)
 }
 
 func bytesToUint16(bytes []byte) []uint16 {
@@ -109,6 +131,12 @@ func bytesToUint16(bytes []byte) []uint16 {
 		values[i] = binary.BigEndian.Uint16(bytes[i*2 : (i+1)*2])
 	}
 	return values
+}
+
+func uint16ToBytes(data uint16) (high, low byte) {
+	bytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(bytes[:2], data)
+	return bytes[0], bytes[1]
 }
 
 func GetPort() []string {
